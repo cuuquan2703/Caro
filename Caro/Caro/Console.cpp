34a862017c0cf34be7b59dc70855057b6debda17
C Quan <cuuquan2703@gmail.com>
2020-11-22 13:50:40 +0700

    Add Control and Enter function

--- Caro/Caro/Console.cpp
+++ Caro/Caro/Console.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<Windows.h>
+#include <conio.h>
 using namespace std;
 #define BOARD_SIZE 12
 #define LEFT 3
@@ -9,7 +10,7 @@ struct _POINT {
 	int x, y, c;
 } _A[BOARD_SIZE][BOARD_SIZE];
 bool TURN;
-int _COMMAND;
+char _COMMAND;
 int _X, _Y;
 
 void FixConsoleWindow() {
@@ -55,9 +56,73 @@ void starGame() {
 	Draw(BOARD_SIZE);
 };
 
+int Checkboard(int pX, int pY) {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (_A[i][j].x == pX && _A[i][j].y == pY && _A[i][j].c == 0) {
+				if (TURN == true) _A[i][j].c = -1;
+				else _A[i][j].c = 1;
+				return _A[i][j].c;
+			}
+		}
+	}
+	return 0;
+};
+
+void MoveRight() {
+	if (_X < _A[BOARD_SIZE - 1][BOARD_SIZE - 1].x) {
+		_X += 4;
+		gotoxy(_X, _Y);
+	}
+};
+
+void MoveLeft() {
+	if (_X > _A[0][0].x) {
+		_X -= 4;
+		gotoxy(_X, _Y);
+	}
+};
+
+void MoveDown() {
+	if (_Y < _A[BOARD_SIZE - 1][BOARD_SIZE - 1].y) {
+		_Y+= 2;
+		gotoxy(_X, _Y);
+	}
+};
+
+void MoveUp() {
+	if (_Y > _A[0][0].y) {
+		_Y -= 2;
+		gotoxy(_X, _Y);
+	}
+};
+
+
 int main() {
 	FixConsoleWindow();
-	statGame();
+	starGame();
+	while (1) {
+		_COMMAND = toupper(_getch());
+		if (_COMMAND == 27)
+		{
+			return 0;
+		}
+		else {
+			if (_COMMAND == 'A') MoveLeft();
+			else if (_COMMAND == 'W') MoveUp();
+			else if (_COMMAND == 'S') MoveDown();
+			else if (_COMMAND == 'D') MoveRight();
+			else if (_COMMAND == 13) {
+				switch ((Checkboard(_X, _Y)))
+				{
+				case -1:
+					cout << "X"; break;
+				case 1:
+					cout << "O"; break;
+				}
+			}
+		}
+	}
 	return 0;
 }
 
