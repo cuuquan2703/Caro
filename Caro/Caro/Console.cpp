df47889843c67d45ca99774ab98e8d04158f06bd
C Quan <cuuquan2703@gmail.com>
2020-11-22 11:56:22 +0700

    variable

--- Caro/Caro/Console.cpp
+++ Caro/Caro/Console.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 #include<Windows.h>
 using namespace std;
+#define BOARD_SIZE 12
+#define LEFT 3
+#define TOP 1
+
+struct _POINT {
+	int x, y, c;
+} _A[BOARD_SIZE][BOARD_SIZE];
+bool TURN;
+int _COMMAND;
+int _X, _Y;
+
 void FixConsoleWindow() {
 	HWND consoleWindow = GetConsoleWindow();
 	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
